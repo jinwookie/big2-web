c474ed56d55fa7d27ecfa5c383f9441a60738367
jinwookie <jinwookie@hotmail.com>
2017-02-14 08:42:39 -0500

    fix layout of dashboard

--- src/client/components/dashboard/DashboardTable.js
+++ src/client/components/dashboard/DashboardTable.js
@@ -9,9 +9,11 @@ const DashboardTable = ({
       dashboard.map((player, index) =>
         <div key={index} className="dashboard-player">
           <h2>{ `${player.firstname} ${player.lastname}` }</h2>
-          <div>Total: <strong>{ player.total }</strong></div>
-          <div>Games Played: <strong>{ player.games }</strong></div>
-          <div>Average: <strong>{ Math.round((player.total / player.games) * 100) / 100 }</strong></div>
+          <div className="dashboard-stats">
+            <div><span>Total:</span> <strong>{ player.total }</strong></div>
+            <div><span>Games Played:</span> <strong>{ player.games }</strong></div>
+            <div><span>Average:</span> <strong>{ Math.round((player.total / player.games) * 100) / 100 }</strong></div>
+          </div>
         </div>
       )
     }
